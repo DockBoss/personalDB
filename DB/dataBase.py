95a2d6a8c2339a3d2b29323e7f57a10884efb9e7
DockBoss <jareddoro@gmail.com>
2021-10-14 15:44:57 -0500

    same

--- DB/dataBase.py
+++ DB/dataBase.py
@@ -1,48 +1,50 @@
 import mysql.connector
 from mysql.connector import errorcode
 
-myDB = None
+#create class for connection object with various function 
+class Connection:
+  def __del__(self):
+    self.myDB.close()
 
-def connect(userName, password):
-  try:
-    connector = mysql.connector.connect(user= userName, 
-                                    password= password, 
-                                    host= '127.0.0.1', 
-                                    database= 'todo',
-                                    use_pure=True)
-
-  except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-      print("Something is wrong with your user name or password")
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-      print("Database does not exist")
+  def __init__(self, userName, password):
+    try:
+      connector = mysql.connector.connect(user= userName, 
+                                      password= password, 
+                                      host= '127.0.0.1', 
+                                      database= 'todo',
+                                      use_pure=True)
+  
+    except mysql.connector.Error as err:
+      if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+      elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+      else:
+        print(err)
     else:
-      print(err)
-  else:
-      print("Connected")
-      myDB = connector
+        print("Connected")
+        self.myDB = connector
 
+        '''
+        create cursors, not sure why they should be buffered
+        I read a little on it but was confused
+        I am not sure If these should be used for every function but I will use it
+        that way
+        '''
+        self.myCursor1 = self.myDB.cursor(buffered = True)
+        self.myCursor2 = self.myDB.cursor(buffered = True)
 
-'''
-create cursors, not sure why they should be buffered
-I read a little on it but was confused
-I am not sure If these should be used for every function but I will use it
-that way
-'''
+  def toggleState(self, compleated):
+  #get active tasks
+    query = ("SELECT description, listId FROM task WHERE compleated = %s")
+    value = (compleated,)
+    self.myCursor1.execute(query,value)
+    self.printDB(self.myCursor1)
 
-myCursor1 = myDB.cursor(buffered = True)
-myCursor2 = myDB.cursor(buffered = True)
-
-def toggleState(compleated):
-#get active tasks
-  query = ("SELECT description, listId FROM task WHERE compleated = %s")
-  value = (compleated,)
-  myCursor1.execute(query,value)
-
-#tasks = myCursor1.fetchall()
-
-for x in myCursor1:
-  print(x)
+  def printDB(cursor):
+    for x in cursor:
+      print(x)
 
 
 #myDB.close()
+#tasks = myCursor1.fetchall()
